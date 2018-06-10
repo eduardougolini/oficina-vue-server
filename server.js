f795f0f017e6caf3da1c9de984553627ee9f5cdc
Eduardo Ugolini <edu.ugolini2@gmail.com>
2018-06-10 18:49:44 -0300

    Adicionado backend da oficina

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(cors());
+app.use(bodyParser());
+
+
+app.post('/login', (req, res, next) => {
+    let email = req.body.email;
+    let senha = req.body.senha;
+
+    let authenticated = false;
+
+    if (email === 'email@email.com' && senha === 'senha') {
+        authenticated = true;
+    }
+
+    res.json({ 'authenticated': authenticated });
+})
+
+app.listen(8081, function () {
+    console.log(':)');
+});
